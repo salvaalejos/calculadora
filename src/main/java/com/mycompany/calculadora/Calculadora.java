6dca8e962af949bcfb2e7b0b2bcf936974f0e6a8
salvaalejos <alejossalvador@gmail.com>
2024-09-08 00:40:39 -0600

    contenido

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import Views.CalculadoraGUI;
+
+/**
+ *
+ * @author hfyh
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        CalculadoraGUI calculadora = new CalculadoraGUI();
+        calculadora.setSize(380, 640);
+        calculadora.setLocationRelativeTo(null);
+        calculadora.setVisible(true);
+    }
+}
